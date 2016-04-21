06c2cdc3249b765516b23bd83dfad5c5d8708e8e
justin-ch-goh <justin.ch.goh@gmail.com>
2016-04-22 01:19:38 +0800

    added typhoeus for parallel requests

--- q10mobileDevicesMobileAccessories.rb
+++ q10mobileDevicesMobileAccessories.rb
@@ -1,11 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
-require 'typhoeus'
 
 
 # Fetch and parse HTML document
-doc = Nokogiri::HTML(open("http://list.qoo10.sg/gmkt.inc/Bestsellers/"))
+doc = Nokogiri::HTML(open("http://list.qoo10.sg/gmkt.inc/Category/Default.aspx?gdlc_cd=100000014&gdmc_cd=200000323&pageSize=120&curPage=70"))
 # path = doc.css("li#441633906 a")[0]['href']
 # section = doc.css("div.section_ctlst li")[0]
 # puts section
@@ -24,45 +23,29 @@ discountDel = []  # strikethrough
 
 reviews = []      # go into the product's URL, extract the first review
 
-itemUrls = []
-reviewsTypheous = []
-
-doc.css("div.section_ctlst li").each do |link|
+doc.css("div.bd_glr4 li").each do |link|
 
   item_url = link.css("a.thumb")[0]['href']
-
-  itemUrls << item_url
 
   paths << item_url
   thumbnailImg << link.css("a.thumb img")[0]['gd_src'] # link.css("a.thumb img")[0]['src'] didn't work due to spinner
   title << link.css("p.subject a")[0]['title']
-  price << link.css("p.price strong")[0].text
+  price << link.css("div.price strong")[0].text
   sellerShop << link.css("p.name a")[0].text
-  shipping << link.css("div.shipping p").text
+  shipping << link.css("div.shipping span").text
   discountEm << link.css("p.discount em").text
   discountSpan << link.css("p.discount span").text
-  discountDel << link.css("p.discount del").text
+  discountDel << link.css("div.price del").text
 
-  # requests in series
   # extract reviews from each page
-  # item_page = Nokogiri::HTML(open("#{item_url}"))
-  # review = item_page.css("ul.pht_review dd.detail a").text
-  # puts review
-  # reviews << review
+  item_page = Nokogiri::HTML(open("#{item_url}"))
+  review = item_page.css("ul.pht_review dd.detail a").text
+  puts review
+  reviews << review
   
 end
 
-# using Typhoeus to run multiple requests in parallel to de-bottleneck Nokogiri
-hydra = Typhoeus::Hydra.new(max_concurrency: 1)
-requests = itemUrls.map { |path| Typhoeus::Request.new("#{path}") }
-requests.each { |request| hydra.queue(request) }
-hydra.run
-requests.each { |request| 
-  review = Nokogiri::HTML(request.response.body).css("ul.pht_review dd.detail a").text
-  # puts review
-  reviews << review }
-
-CSV.open("q10bestsellers.csv", "w") do |csv|
+CSV.open("q10mobileDevicesMobileAccessories.csv", "w") do |csv|
   puts "Writing to csv..."
   csv << paths
   csv << thumbnailImg
